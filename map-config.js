7061b73de345898fe3b5646dc65dcdd3176d7aa0
David Christensen <outlaw.dave@gmail.com>
2021-11-07 22:13:41 -0600

    Add files via upload

---
+++ map-config.js
@@ -0,0 +1,82 @@
+﻿
+var tryjsconfig = {
+  "tryjs1":{
+    "hover": "AREA #1",
+    "url": "",
+    "target": "custom",
+    "upColor": "#FFFFF3",
+    "overColor": "#ECFFB3",
+    "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs2":{
+    "hover": "AREA #2",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs3":{
+    "hover": "AREA #3",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs4":{
+    "hover": "AREA #4",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs5":{
+    "hover": "AREA #5",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs6":{
+    "hover": "AREA #6",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs7":{
+    "hover": "AREA #7",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs8":{
+    "hover": "AREA #8",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs9":{
+    "hover": "AREA #9",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs10":{
+    "hover": "AREA #10",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs11":{
+    "hover": "AREA #11",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "tryjs12":{
+    "hover": "AREA #12",
+    "url": "", "target": "custom",
+    "upColor": "#FFFFF3", "overColor": "#ECFFB3", "downColor": "#cae9af",
+    "active": true
+  },
+  "general":{
+    "borderColor": "black",
+    "visibleNames": "black"
+  }
+};
